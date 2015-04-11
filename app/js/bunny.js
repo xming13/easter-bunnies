5d997d8106c9943a13bbd87ece086161288664ca
xming13 <xiaoming33@gmail.com>
2015-04-11 21:27:34 +0800

    Fix data

--- app/js/bunny.js
+++ app/js/bunny.js
@@ -388,9 +388,9 @@ XMing.GameStateManager = new function() {
             easterEgg: {
                 allGames: false,
                 allLeaderboard: false,
-                findTheWord: false,
-                followTheNumbers: false,
-                spotTheSpecialOne: false,
+                word: false,
+                numbers: false,
+                specialOne: false,
                 mushrooms: false,
                 squirrel: false
             },
