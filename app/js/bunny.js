0419c4ea361e0d5a5aeb70d12ee7de73d4ad96e1
xming13 <xiaoming33@gmail.com>
2015-05-09 17:21:07 +0800

    New layout

--- app/js/bunny.js
+++ app/js/bunny.js
@@ -103,6 +103,14 @@ XMing.GameStateManager = new function() {
             confirmButtonColor: '#3BD9F5'
         });
 
+        $(".mfb-component__button--main").click(function() {
+            if ($(this).parent().parent().attr('data-mfb-state') === "open") {
+                $(".overlay").addClass("active");
+            } else {
+                $(".overlay").removeClass("active");
+            }
+        });
+
         $(".btn-play").click(function() {
             self.startGame();
         });
